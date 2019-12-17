f1e6b22bc708f323c88432e78965bb0f51c1c57f
luanloose <luanloose@hotmail.com>
2019-12-16 23:11:32 -0300

    Pasta com exemplos web
    HTML, CSS e javascript

---
+++ Projetos Web/Javascript/meu_script.js
@@ -0,0 +1,15 @@
+//alert('Olá, seja bem vindo ao curso!!!')
+
+/*
+	selecionar um elemento no DOM
+	atualizar o valor desse elemento com uma string
+*/
+var a = 10
+var b = 20
+
+b = b-a
+a = a + b
+
+document.write('A: ' + a + '<br/>')
+document.write('B: ' + b + '<br/>')
+document.write('<hr/>')
